c5bbd5fbcc49fc17f5c40d635feb1fd1addaf931
Dag Brattli <dag@brattli.net>
2018-09-12 12:47:00 +0200

    Cleanup

--- Sources/AsyncObserver.fs
+++ Sources/AsyncObserver.fs
@@ -5,7 +5,7 @@ module AsyncObserver =
     type AsyncObserver<'a> = AsyncObserver of Types.AsyncObserver<'a> with
         static member Unwrap (AsyncObserver obv) : Types.AsyncObserver<'a> = obv
 
-        member this.OnNextAsync (x : 'a) = AsyncObserver.Unwrap this <| OnNext x
+        member this.OnNextAsync (x: 'a) = AsyncObserver.Unwrap this <| OnNext x
         member this.OnErrorAsync err = AsyncObserver.Unwrap this <| OnError err
         member this.OnCompletedAsync () = AsyncObserver.Unwrap this <| OnCompleted
 

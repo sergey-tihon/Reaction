5717717f22a0a40df0a0d49dab9964f5096d72bf
Dag Brattli <dag@brattli.net>
2018-09-15 08:46:08 +0200

    Added more members for async disposable

--- Sources/AsyncDisposable.fs
+++ Sources/AsyncDisposable.fs
@@ -1,7 +1,11 @@
 namespace Reaction
 
 type AsyncDisposable = AsyncDisposable of Types.AsyncDisposable with
-        static member Unwrap (AsyncDisposable dsp) : Types.AsyncDisposable = dsp
+    static member Unwrap (AsyncDisposable dsp) : Types.AsyncDisposable = dsp
 
-        member this.DisposeAsync () = AsyncDisposable.Unwrap this ()
+    static member Empty = AsyncDisposable Core.disposableEmpty
+
+    static member Composite seq = AsyncDisposable <| Core.compositeDisposable seq
+
+    member this.DisposeAsync () = AsyncDisposable.Unwrap this ()
 
